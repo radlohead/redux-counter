ccde1ddc60d767072a307262af39c642a5d8487b
박민호 <radlohead@naver.com>
2018-12-17 21:54:23 +0900

    README.md 수정

--- src/actions/ActionTypes.ts
+++ src/actions/ActionTypes.ts
@@ -1,3 +1,3 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const SET_COLOR = 'SET_COLOR';
+export const INCREMENT: string = 'INCREMENT';
+export const DECREMENT: string = 'DECREMENT';
+export const SET_COLOR: string = 'SET_COLOR';
